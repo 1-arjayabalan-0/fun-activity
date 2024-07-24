c09e808b1a2c20c1848019b943003c34f33cad9b
unknown <arjayabalan09@gmail.com>
2024-07-25 00:32:11 +0530

    data updated

--- index.js
+++ index.js
@@ -89,3 +89,19 @@ $(document).ready(function () {
     }
 
 })
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeTextColor() {
+    const textElement = document.getElementById('ft_text');
+    textElement.style.color = getRandomColor();
+}
+
+setInterval(changeTextColor, 500);
